ce03803e7e481e3131c8ee891aedaeda23ed55a7
Lindsay <Lindsay.Wells@utas.edu.au>
2019-01-29 16:29:13 +1100

    Added Firebase Realtime DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:barcode_scan/barcode_scan.dart';
 
-void main() => runApp(MyApp());
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+
+import 'dart:async';
+import 'dart:io' show Platform; //needed for firebase.configure() platform specific infos
+
+//converted main into an async, so that we can set up the firebase connection first
+//before running the app proper:
+Future<void> main() async
+{
+  //connect to firebase
+  final FirebaseApp app = await FirebaseApp.configure(
+    name: 'db2',
+    options: Platform.isIOS
+        ? const FirebaseOptions(
+            googleAppID: '1:222742060511:ios:cd7b863a4b3342ea',
+            gcmSenderID: '222742060511',
+            databaseURL: 'https://flutterqr-5af12.firebaseio.com',
+          )
+        : const FirebaseOptions(
+            googleAppID: '1:222742060511:android:cd7b863a4b3342ea',
+            apiKey: 'AIzaSyCjxvgsgOQDukPnv0FCgA1qXzuPedSSXu0',
+            databaseURL: 'https://flutterqr-5af12.firebaseio.com',
+          ),
+  );
+
+  //now just the usual main() stuff of running the app (widget)
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -44,21 +75,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String barcode = "";
+
+  final TextEditingController textEditingController = new TextEditingController();
+  String enteredText = "";
+
+
+  Future _scan() async
+  {
+    String barcode = await BarcodeScanner.scan();
+    setState(() => this.barcode = barcode);
+  }
+  void _handleTextChange()
+  {
+    setState(() => this.enteredText = textEditingController.text);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Start listening to changes
+    textEditingController.addListener(_handleTextChange);
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is removed from the Widget tree
+    // This also removes the _printLatestValue listener
+    textEditingController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -75,37 +127,20 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            RaisedButton(
+                onPressed: _scan,
+                child:Text("Scan A Code")
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Text("Scanned text was: "+this.barcode),
+            TextField(
+              controller: textEditingController,
             ),
+            QrImage(data: this.enteredText)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
